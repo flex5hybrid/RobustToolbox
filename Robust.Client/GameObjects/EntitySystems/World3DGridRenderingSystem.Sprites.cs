2a3d13d1c068caae2df690ca41dc4b31e7368b6b
Foxxpress <113517899+flex5hybrid@users.noreply.github.com>
2026-09-02 00:03:10 +0300

    Fix sprite billboard direction extensions

--- Robust.Client/GameObjects/EntitySystems/World3DGridRenderingSystem.Sprites.cs
+++ Robust.Client/GameObjects/EntitySystems/World3DGridRenderingSystem.Sprites.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Numerics;
 using Robust.Client.Graphics;
+using Robust.Client.Utility;
 using Robust.Shared.Graphics.RSI;
 using Robust.Shared.Maths;
 
